0186b840d2e16601c0c79887c0441cb310578621
sun00120 <sun00120@algonquinlive.com>
2022-01-21 14:18:02 -0500

    add js folder, add js file with an array of all songs details (title, source, image, artist name)

---
+++ js/control.js
@@ -0,0 +1,38 @@
+const SONGS = [
+  {
+    title: "DDU-DU DDU-DU",
+    src: "media/song/BLACKPINK-DDU-DU DDU-DU.mp3",
+    img: "media/img/BLACKPINK-DDU-DU DDU-DU.jpeg",
+    artist: "BLACKPINK",
+  },
+  {
+    title: "Butter",
+    src: "media/song/BTS-Butter.mp3",
+    img: "media/img/BTS-Butter.jpeg",
+    artist: "BTS",
+  },
+  {
+    title: "WANNABE",
+    src: "media/song/ITZY-WANNABE.mp3",
+    img: "media/img/ITZY-WANNABE.jpeg",
+    artist: "ITZY",
+  },
+  {
+    title: "LILAC",
+    src: "media/song/IU-LILAC.mp3",
+    img: "media/img/IU-LILAC.jpeg",
+    artist: "IU",
+  },
+  {
+    title: "MONEY",
+    src: "media/img/LISA-MONEY.jpeg",
+    img: "media/song/LISA-MONEY.mp3",
+    artist: "LISA",
+  },
+  {
+    title: "The Feels",
+    src: "media/song/TWICE-The Feels.mp3",
+    img: "media/img/TWICE-The Feels.jpeg",
+    artist: "TWICE",
+  },
+];
