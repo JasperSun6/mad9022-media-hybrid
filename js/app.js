23928cd1c30a941807ace4e6436a9774c1a74bc7
sun00120 <sun00120@algonquinlive.com>
2022-01-21 19:54:04 -0500

    update play and paste functions to make the song playing correctly

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 const APP = {
+  currentTrack: 5,
   audio: document.getElementById("song-player"),
   player: document.getElementById("player"),
-  currentTrack: 0,
+  totalTime: document.getElementById("total-time"),
+  currentTime: document.getElementById("current-time"),
+
   init: (ev) => {
+    APP.audio.src = SONGS[APP.currentTrack].src;
+
     let play = document.getElementById("btnPlay");
     play.addEventListener("click", APP.startPlay);
 
@@ -11,11 +16,13 @@ const APP = {
 
     let stop = document.getElementById("stop");
     stop.addEventListener("click", APP.stopPlay);
+
+    APP.audio.addEventListener("durationchange", APP.updateTotalTime);
+    APP.audio.addEventListener("timeupdate", APP.updateCurrentTime);
   },
   startPlay: (ev) => {
     if (!APP.audio.paused) return;
     console.log("Song is playing");
-    APP.audio.src = SONGS[APP.currentTrack].src;
     APP.audio.play();
   },
 
@@ -30,8 +37,18 @@ const APP = {
     APP.audio.currentTime = 0;
   },
 
-  totalTime: (ev) => {
+  covertTime: (second) => {
+    return (second - (second %= 60)) / 60 + (9 < second ? ":" : ":0") + second;
+  },
+
+  updateTotalTime: (ev) => {
     console.log("Total time updated");
+    APP.totalTime.innerHTML = APP.covertTime(parseInt(APP.audio.duration));
+  },
+
+  updateCurrentTime: (ev) => {
+    console.log("Current time updated");
+    APP.currentTime.innerHTML = APP.covertTime(parseInt(APP.audio.currentTime));
   },
 };
 
