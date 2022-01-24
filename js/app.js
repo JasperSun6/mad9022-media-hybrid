ca667951512aa6328ad53b1565b772ee55f67e2f
sun00120 <sun00120@algonquinlive.com>
2022-01-24 01:41:49 -0500

    Clean all files' code, add a few new objects and comments

--- js/app.js
+++ js/app.js
@@ -1,51 +1,48 @@
 const INIT = {
   songs: document.querySelectorAll(".song"),
+
   init: (ev) => {
     APP.audio.src = SONGS[APP.currentTrack].src;
 
     APP.play.addEventListener("click", APP.startPlay);
+    APP.stop.addEventListener("click", APP.stopPlay);
 
-    let stop = document.getElementById("stop");
-    stop.addEventListener("click", APP.stopPlay);
-
-    APP.audio.addEventListener("durationchange", APP.updateTotalTime);
-    APP.audio.addEventListener("timeupdate", APP.updateCurrentTime);
+    APP.audio.addEventListener("durationchange", TIME.updateTotalTime);
+    APP.audio.addEventListener("timeupdate", TIME.updateCurrentTime);
 
     INIT.songs.forEach((song) => {
-      song.addEventListener("click", APP.songSelected);
+      song.addEventListener("click", HIGHLIGHT.songSelected);
     });
   },
 };
 
 const APP = {
   currentTrack: 0,
-  audio: document.getElementById("song-player"),
+  play: document.getElementById("btnPlay"),
+  stop: document.getElementById("stop"),
+  audio: document.getElementById("audio-player"),
   player: document.getElementById("player"),
   totalTime: document.getElementById("total-time"),
   currentTime: document.getElementById("current-time"),
-  play: document.getElementById("btnPlay"),
 
   // play song
   startPlay: (ev) => {
     APP.audio.play();
     APP.updateToPause();
-    APP.playAnimation();
+    ANIMATION.playAnimation();
   },
-
-  // update button to pause button
-  updateToPause: () => {
-    APP.play.removeEventListener("click", APP.startPlay);
-    APP.play.id = "pause";
-    APP.play.innerHTML = "pause";
-    let pause = document.getElementById("pause");
-    pause.addEventListener("click", APP.pausePlay);
-  },
-
   // pause song
   pausePlay: (ev) => {
     APP.audio.pause();
     APP.updateToPlay();
-    APP.pauseAnimation();
+    ANIMATION.pauseAnimation();
+  },
+  // stop playing
+  stopPlay: (ev) => {
+    APP.audio.pause();
+    APP.audio.currentTime = 0;
+    APP.updateToPlay();
+    ANIMATION.stopAnimation();
   },
 
   // update button to play button
@@ -56,31 +53,38 @@ const APP = {
     APP.play.innerHTML = "play_arrow";
     APP.play.addEventListener("click", APP.startPlay);
   },
-
-  // stop playing
-  stopPlay: (ev) => {
-    APP.audio.pause();
-    APP.audio.currentTime = 0;
-    APP.updateToPlay();
-    APP.stopAnimation();
+  // update button to pause button
+  updateToPause: () => {
+    APP.play.removeEventListener("click", APP.startPlay);
+    APP.play.id = "pause";
+    APP.play.innerHTML = "pause";
+    let pause = document.getElementById("pause");
+    pause.addEventListener("click", APP.pausePlay);
   },
+};
 
-  //Code learned from slackoverflow - credit GitaarLAB
+const TIME = {
   //https://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds
+  //Code learned from slackoverflow - credit to GitaarLAB
+  // Cover seconds to minutes and seconds
   covertTime: (seconds) => {
     return (
       (seconds - (seconds %= 60)) / 60 + (9 < seconds ? ":" : ":0") + seconds
     );
   },
-
-  updateTotalTime: (ev) => {
-    APP.totalTime.innerHTML = APP.covertTime(parseInt(APP.audio.duration));
-  },
-
+  // update track's currentTime
   updateCurrentTime: (ev) => {
-    APP.currentTime.innerHTML = APP.covertTime(parseInt(APP.audio.currentTime));
+    APP.currentTime.innerHTML = TIME.covertTime(
+      parseInt(APP.audio.currentTime)
+    );
   },
+  //update track's totalTime
+  updateTotalTime: (ev) => {
+    APP.totalTime.innerHTML = TIME.covertTime(parseInt(APP.audio.duration));
+  },
+};
 
+const HIGHLIGHT = {
   // highlight the song that clicked
   songSelected: (ev) => {
     let listItems = ev.target.closest(".song");
@@ -89,8 +93,10 @@ const APP = {
     });
     listItems.classList.add("active");
   },
+};
 
-  // activated / Deactivated
+const ANIMATION = {
+  // activated animation
   playAnimation: (ev) => {
     for (let i = 1; i < 10; i++) {
       let visual = document.querySelector(`.r-${i}`);
@@ -98,12 +104,14 @@ const APP = {
       visual.style.animationPlayState = "running";
     }
   },
+  // paused animation
   pauseAnimation: (ev) => {
     for (let i = 1; i < 10; i++) {
       let visual = document.querySelector(`.r-${i}`);
       visual.style.animationPlayState = "paused";
     }
   },
+  // deactivated animation
   stopAnimation: (ev) => {
     for (let i = 1; i < 10; i++) {
       let visual = document.querySelector(`.r-${i}`);
